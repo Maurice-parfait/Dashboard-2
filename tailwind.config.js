6d16111288a533973f6aec5e3649896568506beb
maurice <mauricemiema90@gmail.com>
2024-07-01 20:59:40 +0200

    dasboard en cours

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html"],
+  content: ["./index.html", "chart.js"],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns : {
+        'grid1' : '1.2fr 5fr',
+        'gridinterne1' : '1fr 1fr 1fr 1fr',
+        'chart' : '5fr 3fr',
+      },
+    },
   },
   plugins: [],
 }
